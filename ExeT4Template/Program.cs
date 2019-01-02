1fe5ff80e52e77bb41fb393501d8691815c8549e
tharo <tharopan@gmail.com>
2019-01-03 00:03:47 +0530

    Hello world

---
+++ ExeT4Template/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExeT4Template
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            HelloWorldTemplate page = new HelloWorldTemplate();
+            String pageContent = page.TransformText(); 
+
+            System.IO.File.WriteAllText("outputPage.html", pageContent);
+
+            Console.ReadKey();
+        }
+    }
+}
